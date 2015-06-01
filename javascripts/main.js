0168a2cc3ded32f79c9b8c78faa9d5c1ef869305
ling <1261283072@qq.com>
2015-06-01 22:23:23 +0800

    6.1end

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
 console.log('This would be the main JS file.');
+window.onload=function(){
+	var oTime=document.getElementById('time');
+	var oDate=new Date();
+
+	var oYear=oDate.getFullYear();
+	var oMonth=oDate.getMonth()+1;
+	var oDay=oDate.getDate();
+	var oHours=oDate.getHours();
+	var oMinutes=oDate.getMinutes();
+	var oSeconds=oDate.getSeconds();
+
+	oTime.innerHTML=oYear+"-"+oMonth+"-"+oDay+" "+oHours+":"+oMinutes+":"+oSeconds;
+	console.log(1);
+}
